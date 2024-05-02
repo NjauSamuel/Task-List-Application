d55986007841425641e4d4c1cca4067134d8305a
NjauSamuel <njaus602@gmail.com>
2024-05-02 13:56:35 +0300

    Added beariful styling to the layout and the index pages, more is still yet to come!

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,12 +3,14 @@
 
 <head>
   <title>Laravel 10 Task List App</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+
   @yield('styles')
 </head>
 
-<body>
+<body class="container mx-auto mt-10 mb-10 max-w-lg">
 
-  <h1>@yield('title')</h1>
+  <h1 class="text-2xl mb-4">@yield('title')</h1>
 
   <div>
 
